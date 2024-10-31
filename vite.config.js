e1358c0aaf4ab276112d599a78ba76583ac15869
shiva <s3r3shiva@gmail.com>
2024-10-31 18:10:32 +0530

    everything completed

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/react-portfolio/',  // Replace 'repo-name' with your repository name
   build: {
-    outDir: 'build', // Change the output directory to 'build'
+    outDir: 'build',
   },
 });
+
