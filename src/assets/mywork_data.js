0f21b1a09825ab324292dc7afcf857130bbc1e8a
shiva <s3r3shiva@gmail.com>
2024-11-03 22:00:39 +0530

    added both movieverese and fashionfoot hosted links

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -15,14 +15,17 @@ const mywork_data=[
         w_no:1,
         work_name:"FashionFoot",
         work_img:img1,
-        worked_with:[python, django]
+        worked_with:[python, django],
+        git_link:"https://fashionfoot-production.up.railway.app/"
     },
 
     {
         w_no:2,
         work_name:"MOVIEVERSE",
         work_img:img2,
-        worked_with:[python, django]
+        worked_with:[python, django],
+        git_link:"https://movieverse.up.railway.app/"
+
     },
 
     {
